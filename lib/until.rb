2f31eaa1a2e5567be96233eefda593a68672cc63
Marcus Shimotsu <marcus.shimotsu@gmail.com>
2020-12-31 21:07:17 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
   until levitation_force == 10
-    puts "Leviation force is #{levitation_force}"
+    puts "Wingardium Leviosa"
     levitation_force += 1
   end
 end
